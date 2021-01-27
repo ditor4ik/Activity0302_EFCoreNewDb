e87b154baff4bbf3e2859fb4daeb041b80eae73f
ditor <avdiunin.d@yandex.ru>
2021-01-27 12:49:54 +0300

    Добавьте файлы проекта.

---
+++ InventoryDatabaseCore/InventoryDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace InventoryDatabaseCore
+{
+    public class InventoryDbContext : DbContext
+    {
+        static IConfigurationRoot _configuration;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: true,
+                        reloadOnChange: true);
+                _configuration = builder.Build();
+                var cnstr = _configuration.GetConnectionString("InventoryManager");
+                optionsBuilder.UseSqlServer(cnstr);
+            }
+        }
+
+        public DbSet<Item> Items { get; set; }
+
+        public InventoryDbContext(): base() { }
+        public InventoryDbContext(DbContextOptions options)
+            : base()
+        {
+
+        }
+    }
+}
